4379d8ce1390aa4403a9ad3d8281315d680e2667
mehak-sachdeva <ms4978@columbia.edu>
2019-09-02 14:49:57 -0700

    review changes

--- Test/NotCSharp/Python/EASY-UniqueEmailAddresses.py
+++ Test/NotCSharp/Python/EASY-UniqueEmailAddresses.py
@@ -1,14 +1,13 @@
 #https://leetcode.com/problems/unique-email-addresses/submissions/
 
 class Solution:
-    def numUniqueEmails(self, emails: List[str]) -> int:
-        i = 0
-        j = len(emails)-1
-        em=[]
-        for email in emails:
-            loc = email.split('@')[0]
-            loc=loc.replace('.','')
-            if '+' in loc:
-                loc = loc[:loc.index('+')]
-            em.append(loc+'@'+email.split('@')[1])
-        return(len(set(em)))
+    def numUniqueEmails(self, emails):
+        setx = set()
+        for e in emails:
+            splits = e.split("@")
+            first = splits[0]
+            last = splits[1]
+            first = first.split("+")[0]
+            first = first.replace('.','')
+            setx.add(first + "@" + last)
+        return len(setx)
