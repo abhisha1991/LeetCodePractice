b53c61483e14008854bb052e90d4b89117241952
Abhi Sharma <abhisha@microsoft.com>
2022-02-04 01:04:08 -0800

    more soln

--- Python/MinStack.py
+++ Python/MinStack.py
@@ -1,36 +1,32 @@
 # https://leetcode.com/problems/min-stack/submissions/
-class MinStack(object):
+class MinStack:
     def __init__(self):
         self.stack = []
-        # holds minimum elements of the stack. 
-        # Top will always be most min element, 2nd from top is 2nd most min element and so on
-        self.minst = []
         
-    def push(self, x):
-        # if we dont have a minimums at all or our min most element is > element being inserted (x)
-        if not self.minst or self.getMin() >= x:
-            self.minst.append(x)
-            
-        self.stack.append(x)
-                
-    def pop(self):
-        # only if the top most element being popped from original stack is the min most element
-        # only then does the min most element get updated
-        if self.stack and self.minst and self.stack[-1] == self.getMin():
-                self.minst = self.minst[:-1]
-            
-        if self.stack:
-            self.stack = self.stack[:-1]
+    def push(self, val: int) -> None:
+        if self.stack == []:
+            self.stack.append([val, val])
+        else:
+            prev = self.stack[-1]
+            # store every stack element as [value, min_so_far]
+            # this is the key point
+            # in a stack, say if we have [5,7,2,3] -- min is 2 up to element 3
+            # so the idea is that along with element addition in stack, we store "min up to that top of stack" element
+            # so we store the above like [[5,5], [7,5], [2,2], [3,2]] -- 1st element is val, 2nd element is min element so far 
+            self.stack.append([val, min(val, prev[1])])
 
-    def top(self):
-        if self.stack:
-            return self.stack[-1]
-        return None
+    def pop(self) -> None:
+        if self.stack == []:
+            return
+        self.stack.pop()
 
-    def getMin(self):
-        if self.minst:
-            return self.minst[-1]
-        return None
+    def top(self) -> int:
+        last = self.stack[-1]
+        return last[0]
+
+    def getMin(self) -> int:
+        last = self.stack[-1]
+        return last[1]
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
