7f7ad98680658ddfcf0ea88c7990f210bba13356
Abhi Sharma <abhisha@microsoft.com>
2022-02-06 15:43:19 -0800

    more soln

--- Python/REDO-SerializeDeserializeTree.py
+++ Python/REDO-SerializeDeserializeTree.py
@@ -1,6 +1,5 @@
 # https://www.youtube.com/watch?v=jwzo6IsMAFQ
 # https://leetcode.com/problems/serialize-and-deserialize-binary-tree/
-# worst case below algo is o(n2), o(n) is given here: https://www.youtube.com/watch?v=1pOqgdO327Q
 class TreeNode(object):
     def __init__(self, x):
         self.val = x
@@ -9,57 +8,71 @@ class TreeNode(object):
 
 class Codec:
     def __init__(self):
-        self.serial = []
         self.idx = 0
-
-    def serialize(self, root):
-        """Encodes a tree to a single string.
-
-        :type root: TreeNode
-        :rtype: str
-        """
-        self.shelper(root)
-        print(self.serial)
-        return self.serial
-
-    def shelper(self, root):
-        if root is None:
-            # this step is key, to append the null values as '#'
-            self.serial.append(str("#"))
+        self.n = -1
+        self.serial = []
+        
+    def serialize_rec(self, root):
+        if root == None:
+            self.serial.append(None)
             return
-
+        
         # note how we are doing a pre order serializer, this is important as it would be linked to the deserializer
-        self.serial.append(str(root.val))
-        self.shelper(root.left)
-        self.shelper(root.right)
-
-    def deserialize(self, data):
-        """Decodes your encoded data to tree.
-
-        :type data: str
-        :rtype: TreeNode
-        """
-        if len(data) == 0:
-            return []
-
-        return self.dhelper(data)
-
-    def dhelper(self, data):
-        if self.idx == len(data) or data[self.idx] == '#':
-            self.idx += 1
+        
+        self.serial.append(root.val)
+        self.serialize_rec(root.left)
+        self.serialize_rec(root.right)
+        return self.serial
+    
+    def deserialize_rec(self, data):
+        if self.idx >= self.n:
+            return
+        
+        if data[self.idx] == None:
+            self.idx +=1
             return None
-
+        
         # note how the deserializer also does a pre-order construction, same as serializer
         # note that pre-order is convenient, as first element of array is root
         # if we did postorder, then we would have to start from the other side 'LRN',
         # worse if we did inorder, we will need to figure out where to start and we cant increment index simply to get left/right subtree
         root = TreeNode(data[self.idx])
-        self.idx += 1
-        root.left = self.dhelper(data)
-        root.right = self.dhelper(data)
+        
+        self.idx +=1
+        root.left = self.deserialize_rec(data)
+        root.right = self.deserialize_rec(data)
         return root
+    
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        
+        self.serialize_rec(root)
+        return str(self.serial)
 
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        # data is given as a string list -- so convert to comma separated string
+        data = data.strip()
+        data = data.replace('[', '')
+        data = data.replace(']', '')        
+        data = data.replace(' ', '')
+        # now convert sanitized input to list
+        data = data.split(',')
+        data = [x if x != 'None' else None for x in data]
+        self.n = len(data)
+        return self.deserialize_rec(data)
+        
+        
 
 # Your Codec object will be instantiated and called as such:
-# codec = Codec()
-# codec.deserialize(codec.serialize(root))
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
