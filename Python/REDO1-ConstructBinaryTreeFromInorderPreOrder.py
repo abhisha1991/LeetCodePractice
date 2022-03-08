ee1776c5e0fe70c3db526fb6535cdeea0374670f
Abhi Sharma <abhisha@microsoft.com>
2022-03-08 01:17:46 -0800

    more soln

--- Python/REDO1-ConstructBinaryTreeFromInorderPreOrder.py
+++ Python/REDO1-ConstructBinaryTreeFromInorderPreOrder.py
@@ -1,3 +1,4 @@
+# https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, x):
@@ -5,32 +6,31 @@ class TreeNode:
         self.left = None
         self.right = None
 
-# Very elegant solution, had thought of this, but couldnt put it properly into code
-# https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/
-
 class Solution:
-    def buildTree(self, preorder, inorder):
-        """
-        :type preorder: List[int]
-        :type inorder: List[int]
-        :rtype: TreeNode
-        """
-        return self.helper(preorder, inorder)
-
-    def helper(self, preorder, inorder):
-        if inorder:
-            idx = inorder.index(preorder.pop(0))
-            # print("preorder element to be searched", inorder[idx])
-            root = TreeNode(inorder[idx])
-            # print("inorder[0:idx]", inorder[0:idx])
-            # print("inorder[idx+1:]", inorder[idx+1:])
-
-            root.left = self.helper(preorder, inorder[0:idx])
-            # remember the root.right will be using inorder[idx+1:] and NOT inorder[idx:]
-            # why? because idx is the index of root, which has already been considered
-            root.right = self.helper(preorder, inorder[idx + 1:])
-
-            return root
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        return self.buildTreeHelper(preorder, inorder)
+    
+    def buildTreeHelper(self, preOrder, inOrder):
+        if len(inOrder) == 0:
+            return None
+        
+        # notice that in pre-order, 0th element is ALWAYS the root (NLR)
+        rootval = preOrder.pop(0)
+        root = TreeNode(rootval)
+        
+        # we need to get the split point assuming this root value is present in the inOrder
+        # which it always is according to the question
+        idx = inOrder.index(root.val)
+        
+        # for the left part of the tree, we are only allowed to view 0 to idx-1th element of the inorder
+        # remember, preOrder here still contains 0th element as root of the left tree
+        root.left = self.buildTreeHelper(preOrder, inOrder[0:idx])
+        
+        # remember the root.right will be using inorder[idx+1:] and NOT inorder[idx:]
+        # why? because idx is the index of root, which has already been considered
+        # remember, preOrder here still contains 0th element as root of the right tree
+        root.right = self.buildTreeHelper(preOrder, inOrder[idx+1:])
+        return root
 
 s = Solution()
 s.buildTree([100,90,50,40,9,5,10,15,45,99,95,96,200,150,250,225],
