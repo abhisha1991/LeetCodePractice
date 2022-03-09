f10eeae57e70588cf4950a91b9ccad0794f93e08
Abhi Sharma <abhisha@microsoft.com>
2022-03-08 21:55:21 -0800

    more soln

--- Python/ParallelProgrammingDemo/MultiProcessingDemo.py
+++ Python/ParallelProgrammingDemo/MultiProcessingDemo.py
@@ -1,4 +1,5 @@
 '''
+# https://www.youtube.com/watch?v=fKl2JW_qrso
 A multiprocessing system has more than two processors, 
 whereas Multithreading is a program execution technique that allows a single process to have multiple code segments
 '''
@@ -16,8 +17,12 @@ def do_something(seconds):
 
 with concurrent.futures.ProcessPoolExecutor() as executor:
     secs = [5, 4, 3, 2, 1]
+    # processes end up using all the cores available to them
     results = executor.map(do_something, secs)
 
+    # since we're getting back a string from the function, we can access it from results like so
+    for r in results:
+        print(r)
 
 finish = time.perf_counter()
 
