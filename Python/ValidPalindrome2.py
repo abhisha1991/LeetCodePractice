e3bb115d42d2457e2b2d50ab62f79c0f99a5050f
Abhi Sharma <abhisha@microsoft.com>
2022-03-08 19:48:51 -0800

    more soln

--- Python/ValidPalindrome2.py
+++ Python/ValidPalindrome2.py
@@ -1,35 +1,60 @@
-# https://leetcode.com/problems/valid-palindrome-ii/submissions/
-class Solution(object):
-    def validPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        s = list(s)
-        count = 0
-        ls = len(s)
-        i, j = 0, ls-1
-        while i< ls/2:
-            if s[i] != s[j]:
-                return self.ispal(s, i+1,j) or self.ispal(s, i, j-1)
-            i +=1
-            j -=1
-        return True
-    
-    def ispal(self, s, i, j):
-        return s[i:j+1] == list(reversed(s[i:j+1]))   
-
-    # this is the recursive solution
-    # this is not accepted as it is exceeding time, need to investigate why
-    # count stores number of violations where the first and last char were not equal
-    # count starts with 0, is allowed to be maximum ==1
-    def helper(self, s, count, ls):
-        if count > 1:
-            return False
-        if ls <= 1:
+# https://leetcode.com/problems/valid-palindrome-ii
+class Solution:
+    # O(n2) solution
+    def validPalindrome_inefficient(self, s: str) -> bool:
+        if self.isPalindrome(s, 0, len(s)):
             return True
-        if s[0] != s[-1]:
-            return self.helper(s[1:], count+1, ls-1) or \
-        self.helper(s[:-1], count+1, ls-1)
-        else:
-            return self.helper(s[1:-1], count, ls-2)
+        
+        for i in range(len(s)):
+            new_s = s[0:i] + s[i+1:]
+            if self.isPalindrome(str(new_s), 0, len(new_s)):
+                return True
+        return False
+     
+    # this is O(n) solution
+    def validPalindrome(self, s: str) -> bool:
+        # does 2 passes over string, one allowing mismatch to be overlooked from left side
+        # and the other allowing mismatch to be overlooked from right side
+
+        # 2 pointers, start from begin and end, meet at middle
+        i = 0
+        j = len(s) - 1
+        mismatch = 0
+        
+        # FIRST PASS
+        while i <= j:
+            if s[i] != s[j]:
+                # allow mismatch to be overlooked from left side
+                i +=1
+                mismatch +=1
+            else:
+                i +=1
+                j -=1
+        
+        # up to one mismatch allowed
+        if mismatch <=1:
+            return True
+        
+        # reset for 2nd pass
+        i = 0
+        j = len(s) - 1
+        mismatch = 0
+        
+        # SECOND PASS
+        while i <= j:
+            if s[i] != s[j]:
+                # allow mismatch to be overlooked from right side
+                j -=1
+                mismatch +=1
+            else:
+                i +=1
+                j -=1
+        
+        # up to one mismatch allowed
+        if mismatch <=1:
+            return True
+        
+        return False
+    
+    def isPalindrome(self, s, i, j):
+        return s[i:j+1] == s[i:j+1][::-1]
