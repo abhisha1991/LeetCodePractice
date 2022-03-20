596228f327e15153cae821f7a5abed5129307ca7
unknown <aus2101@columbia.edu>
2022-03-19 23:29:58 -0700

    more soln

--- Python/MEDIUM-Tries.py
+++ Python/MEDIUM-Tries.py
@@ -1,87 +1,45 @@
-from collections import defaultdict
+# https://www.youtube.com/watch?v=asbcE9mZz_U
 # https://leetcode.com/problems/implement-trie-prefix-tree/description/
-
-
 class Trie:
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.endofword = False
-        self.dic = defaultdict(Trie)
+        self.isWordEnd = False
+        self.children = dict()
 
-    def insert(self, word):
-        """
-        Inserts a word into the trie.
-        :type word: str
-        :rtype: void
-        """
-        self.inserthelper(word, self)
-        print("inserted", word)
+    def insert(self, word: str) -> None:
+        # start at root
+        cur = self
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = Trie()
+            # remember to move down the trie
+            cur = cur.children[c]
+        # remember to mark word end after going through all characters
+        cur.isWordEnd = True
 
-    def inserthelper(self, word, t):
-        if len(word) == 0:
-            return
-
-        first = word[0]
-        if len(word) == 1:
-            if t.dic[first] == None:
-                t.dic[first] = Trie()
-
-            t.dic[first].endofword = True
-            return
-
-        rest = word[1:]
-        if t.dic[first] == None:
-            t.dic[first] = Trie()
-
-        self.inserthelper(rest, t.dic[first])
-
-    def search(self, word):
-        """
-        Returns if the word is in the trie.
-        :type word: str
-        :rtype: bool
-        """
-        return self.searchhelper(word, self)
-
-    def searchhelper(self, word, t):
-        if len(word) == 0:
-            return True
-
-        first = word[0]
-        if len(word) == 1:
-            if first not in t.dic.keys():
+    def search(self, word: str) -> bool:
+        # start at root
+        cur = self
+        for c in word:
+            # if at any point you're unable to find the key of the word in the tree
+            if c not in cur.children:
                 return False
-            if t.dic[first].endofword == False:
+            cur = cur.children[c]
+        
+        # the word is completely found IFF it is an end of word in the trie
+        return cur.isWordEnd
+
+    # similar to search, with a minor tweak
+    def startsWith(self, prefix: str) -> bool:
+        cur = self
+        for c in prefix:
+            if c not in cur.children:
                 return False
+            cur = cur.children[c]
+        
+        # doesn't matter if its end of word or not, always return true
+        # complete word is also a prefix
+        return True
 
-        rest = word[1:]
-
-        if first not in t.dic.keys():
-            return False
-
-        return self.searchhelper(rest, t.dic[first])
-
-    def startsWith(self, prefix):
-        """
-        Returns if there is any word in the trie that starts with the given prefix.
-        :type prefix: str
-        :rtype: bool
-        """
-        return self.startswithhelper(prefix, self)
-
-    def startswithhelper(self, prefix, t):
-        if len(prefix) == 0:
-            return True
-
-        first = prefix[0]
-
-        if first not in t.dic.keys():
-            return False
-
-        rest = prefix[1:]
-        return self.startswithhelper(rest, t.dic[first])
 
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()
