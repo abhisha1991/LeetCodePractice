ebedc4229724e7a8a2132d588287c0af8c2015c8
unknown <aus2101@columbia.edu>
2022-03-24 00:30:34 -0700

    more soln

--- Python/MEDIUM-ProductExceptSelf.py
+++ Python/MEDIUM-ProductExceptSelf.py
@@ -1,26 +1,23 @@
 # https://www.youtube.com/watch?v=vB-81TB6GUc
+# https://leetcode.com/problems/product-of-array-except-self
 class Solution:
-    def productExceptSelf(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        if len(nums) == 0 or len(nums) == 1:
-            return nums
-
-        prod = 1
-        # initialize array of 1
-        output = [1] * len(nums)
-        # get left product
-        for i in range(1, len(nums)):
-            prod = prod * nums[i - 1]
-            output[i] = prod
-
-        prod = 1
-        # get right product
-        for i in range(len(nums) - 2, -1, -1):
-            prod = prod * nums[i + 1]
-            # make sure you multiply with output[i] in RHS since its value may no longer be 1
-            output[i] = output[i] * prod
-
-        return output
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        l2r = [1] * n
+        r2l = [1] * n
+        res = [1] * n
+        
+        # move left to right and l2r[i] holds all products of numbers up to i-1
+        for i in range(1, n):
+            l2r[i] = nums[i-1] * l2r[i-1]
+        
+        # move right to left and r2l[i] holds all products of numbers from last element to i+1
+        for i in range(n-2, -1, -1):
+            r2l[i] = nums[i+1] * r2l[i+1]
+        
+        # for each element, we have products to its left, and products to its right
+        # multiiply those 2 and we'll have products except self
+        for i in range(n):
+            res[i] = l2r[i] * r2l[i]
+            
+        return res
