1758ef58117a1bdff4e6e43e05d90e69c3595d61
Abhi Sharma <abhisha@microsoft.com>
2022-03-25 10:33:49 -0700

    more soln

--- Python/MEDIUM-ContainerWithMostWater.py
+++ Python/MEDIUM-ContainerWithMostWater.py
@@ -1,35 +1,26 @@
 # https://leetcode.com/problems/container-with-most-water/description/
 # watch this video - https://www.youtube.com/watch?v=cdRaaEYk6tI
-class Solution(object):
-    def maxArea(self, height):
-        """
-        :type height: List[int]
-        :rtype: int
-        """
-        length = len(height)
-        points = []
-        for i in range(length):
-            points.append((i + 1, height[i]))
-
-        maxa = 0
-        i = 0  # left pointer
-        j = length - 1  # right pointer
-        # while left and right dont meet
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        maxar = 0
+        i = 0
+        n = len(height)
+        j = n-1
         while i < j:
-            # find length
-            l = abs(points[i][0] - points[j][0])
-            # find min height of the 2 points
-            h = min([points[i][1], points[j][1]])
-            # max container area = length x height
-            area = l * h
-            if maxa < area:
-                maxa = area
-            # if left side is smaller, increase left pointer
-            if points[i][1] < points[j][1]:
-                i += 1
+            width = j-i
+            
+            # area is capped by min of the 2 heights
+            ar = min(height[i], height[j]) * width
+            if ar > maxar:
+                maxar = ar
+            
+            # which one should we update, i or j
+            # depends on which of the 2 heights were shorter
+            # we want to always strive for longer heights (so we can capture more area), 
+            # so the shorter side index is updated
+            if height[i] > height[j]:
+                j -=1
             else:
-                # if right side is smaller, increase right pointer
-                j -= 1
-
-        return maxa
-
+                i +=1
+                
+        return maxar
