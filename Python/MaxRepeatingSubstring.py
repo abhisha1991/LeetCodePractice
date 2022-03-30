e8e3c9d82149b47e7d5c8f0a73069ca59ce76d92
unknown <aus2101@columbia.edu>
2022-03-30 00:40:44 -0700

    more soln

---
+++ Python/MaxRepeatingSubstring.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/maximum-repeating-substring
+class Solution:
+    def maxRepeating(self, sequence: str, word: str) -> int:
+        if word not in sequence:
+            return 0
+        
+        res = 0
+        # start with word
+        s = word
+        # see if s is in sequence
+        while sequence.count(s):
+            res += 1
+            # if 1 'word' is there
+            # expand s by concating 'word' to s
+            # and try to find updated s (2 words back to back) again in sequence
+            s += word
+            
+        return res
+        
