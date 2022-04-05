83f69d7257cfb607a6e24293bf7d162053048941
unknown <aus2101@columbia.edu>
2022-04-05 01:09:42 -0700

    more soln

--- Python/MaxIncreaseInCitySkyline.py
+++ Python/MaxIncreaseInCitySkyline.py
@@ -1,34 +1,21 @@
 # https://leetcode.com/problems/max-increase-to-keep-city-skyline/description/
 class Solution:
-    def maxIncreaseKeepingSkyline(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
-        m = len(grid)
-        n = len(grid[0])
-        maxrows = []
-        maxcols = []
+    # the algo is pretty simple
+    # we're looking at a given cell, r,c -- for this we take a look at the max height in current row, max height in current col
+    # these are the 2 heights which we can possibly increase by - hr, hc
+    # we need the min of these so as to not disrput the skyline
+    def maxIncreaseKeepingSkyline(self, grid: List[List[int]]) -> int:
         res = 0
-        for i in range(m):
-            maxrow = 0
-            for j in range(n):
-                if grid[i][j] > maxrow:
-                    maxrow = grid[i][j]
-            maxrows.append(maxrow)
+        n = len(grid)
+        # take transpose of the matrix to make it column major
+        # so you can grab a column at once
+        gridT = [[grid[j][i] for j in range(n)] for i in range(n)]
         
-        for j in range(n):
-            maxcol = 0
-            for i in range(m):
-                if grid[i][j] > maxcol:
-                    maxcol = grid[i][j]         
-            maxcols.append(maxcol)
-        
-        print(maxrows)
-        print(maxcols)
-        for i in range(m):
-            for j in range(n):
-                minrc = min(maxrows[i], maxcols[j])
-                res += minrc - grid[i][j]
+        for r in range(n):
+            for c in range(n):
+                h_minmax = min([max(grid[r]), max(gridT[c])])
+                # remember we're adding the differnce between our curr height and the max allowed from above
+                res += h_minmax - grid[r][c]
+                
         return res
         
