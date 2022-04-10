d263a2edadc19414d5b0def5b1254342ff38b656
unknown <aus2101@columbia.edu>
2022-04-10 14:20:20 -0700

    more soln

--- Python/MEDIUM-NumberOfIslands.py
+++ Python/MEDIUM-NumberOfIslands.py
@@ -1,58 +1,39 @@
 # https://leetcode.com/problems/number-of-islands/description/
-# uses DFS, note the getnbors function - standardize on this function for all nbor related problems
-class Solution(object):
-    def __init__(self):
-        self.count = 0
-        self.visited = []
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+        visit = set()
+        count = 0
+        for r in range(rows):
+            for c in range(cols):
+                grid[r][c] = int(grid[r][c])
+        
+        directions = [[0,1],[1,0],[0,-1],[-1,0]]
+        
+        def dfs(r, c):
+            if r < 0 or c < 0 or r >= rows or c >= cols:
+                return
+            
+            if (r,c) in visit or grid[r][c] == 0:
+                return
+            
+            visit.add((r, c))
+                
+            for d in directions:
+                dfs(r + d[0], c + d[1])
+            
+        
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    
+                    v = len(visit)
+                    dfs(r, c)
 
-    def numIslands(self, grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: int
-        """
-        self.visited = [[False] * len(grid[0]) for x in range(len(grid))]
-
-        m = len(grid)
-        if m == 0:
-            return self.count
-        n = len(grid[0])
-
-        for i in range(m):
-            for j in range(n):
-                if self.visited[i][j]:
-                    continue
-                self.visited[i][j] = True
-                if grid[i][j] == '1':
-                    # print("reached here")
-                    self.helper(grid, i, j)
-                    self.count += 1
-
-        print("count is", self.count)
-        return self.count
-
-    def helper(self, grid, i, j):
-        nbors = self.getnbors(grid, i, j)
-        # print("for i j", i, j)
-        # print("nbors are", nbors)
-        for n in nbors:
-            if self.visited[n[0]][n[1]] is True:
-                continue
-
-            self.visited[n[0]][n[1]] = True
-            if grid[n[0]][n[1]] == '1':
-                self.helper(grid, n[0], n[1])
-
-    def getnbors(self, grid, x, y):
-        temp = []
-        temp.append((x - 1, y))  # north
-        temp.append((x + 1, y))  # south
-        temp.append((x, y + 1))  # east
-        temp.append((x, y - 1))  # west
-        nbors = []
-        for i in temp:
-            if i[0] < 0 or i[1] < 0 or i[0] >= len(grid) or i[1] >= len(grid[0]):
-                continue
-
-            nbors.append(i)
-
-        return nbors
+                    # only when the visit set increases in members
+                    # should we add the number of islands
+                    if len(visit) > v:
+                        count +=1
+        
+        return count
