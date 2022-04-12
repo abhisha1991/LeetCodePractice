a0b9405eb2719d3dfe3157ce5c260546a223f114
unknown <aus2101@columbia.edu>
2022-04-12 11:33:12 -0700

    more soln

--- Python/GasStation.py
+++ Python/GasStation.py
@@ -12,18 +12,18 @@ class Solution:
         
         # also note, there is only ONE start point 
         # which is the earliest point with which we were able to complete the arr pass
-        # without having total go down to less than 0 
+        # without having tank go down to less than 0 
         # https://www.youtube.com/watch?v=lJwbPZGo05A
-        total = 0
+        tank = 0
         start = 0
         for i in range(len(diff)):
-            total += diff[i]
-            if total < 0:
-                # we know current i is not start (since total starting at i < 0), 
+            tank += diff[i]
+            if tank < 0:
+                # we know current i is not start (since tank starting at i < 0), 
                 # so set it to i+1
                 start = i + 1
-                total = 0
+                tank = 0
         
-        # this is the starting position such that the total never dipped below 0
+        # this is the starting position such that the tank never dipped below 0
         # starting from this starting pos
         return start
