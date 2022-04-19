575c62c8450f0d9f2be51d971df975a95e6cfc1b
unknown <aus2101@columbia.edu>
2022-04-19 11:10:58 -0700

    more soln

--- Python/BasicCalculator.py
+++ Python/BasicCalculator.py
@@ -1,65 +1,92 @@
-# https://leetcode.com/problems/basic-calculator-ii/discuss/
+# https://leetcode.com/problems/basic-calculator
 class Solution:
-    def calculate(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        print(s)
-        try:
-            y = int(s)
-            if type(y) == int:
-                return y
-        except:
-            pass
-
-        first = ''
-        second = ''
-        s = s.replace(' ', '')
-        if '/' in s:
-            op = '/'
-        elif '*' in s:
-            op = '*'
-        elif '+' in s:
-            op = '+'
-        elif '-' in s:
-            op = '-'
-        else:
-            raise Exception("op not found")
-        idx = s.index(op)
-
-        # right number
-        for i in range(idx + 1, len(s)):
-            if s[i] >= '0' and s[i] <= '9':
-                second += s[i]
-            else:
-                break
-
-        # left number
-        for i in range(idx - 1, -1, -1):
-            if s[i] >= '0' and s[i] <= '9':
-                first += s[i]
-            else:
-                break
-
-        first = first[::-1]
-        first = int(first)
-        second = int(second)
-
-        if op == '/':
-            res = first / second
-            res = int(res)
-        elif op == '*':
-            res = first * second
-        elif op == '+':
-            res = first + second
-        elif op == '-':
-            res = first - second
-
-        # first op second -> res
-        find = str(first) + op + str(second)
-        s = s.replace(find, str(res))
-        print("Find is", find)
-        print("Res is", res)
-        print("s is", s)
-        self.calculate(s)
+    def calculate(self, s: str) -> int:
+        s = "(" + s + ")"
+        s = s.replace(" ", "")
+        s = list(reversed(s))
+        stack = []
+        op = ["+", "-", "*", "/"]
+        
+        def evaluate(operator, num1, num2):
+            if operator == '+':
+                return num1 + num2
+            elif operator == '-':
+                return num1 - num2
+            elif operator == "*":
+                return num1 * num2
+            elif operator == "/":
+                assert(num2 != 0)
+                return num1/num2
+        
+        i = 0
+        while i < len(s):
+            if s[i] == ')':
+                stack.append(s[i])
+                i +=1
+                continue
+            
+            if s[i].isdigit():
+                j = 0
+                num = int(s[i]) * 10**j
+                i +=1
+                while i < len(s) and s[i].isdigit():
+                    j +=1
+                    num += int(s[i]) * 10**j
+                    i +=1
+                
+                stack.append(num)
+                
+            if i < len(s) and s[i] in op:
+                stack.append(s[i])
+                i +=1
+                continue
+            
+            if s[i] == '(':
+                ele = []
+                while stack and stack[-1] != ')':
+                    # -2 + 1 --> in this case stack is [')', 1, '+', 2, '-']
+                    # so we can have a negative sign in front, to signify negative number, so we need to take care of this
+                    ele.append(stack.pop())
+                    if ele[0] == '-':
+                        ele.pop()
+                        if stack:
+                            n = stack.pop()
+                            assert(type(n) == int)
+                            ele.append(-n)
+                    
+                    # this can happen in the last calculation
+                    # example (7 - 8 + 9) --> )9 + 8 - 7( --> last bracket encountered --> )9 + -1 --> )8
+                    # at this stage, we pop 8 from stack and add 8 to ele and now stack[-1] is )
+                    # so we just want to pop this last ) and break out of the loop, so we can ultimately add 8 to top of stack
+                    if stack and stack[-1] == ')':
+                        # remove the front bracket
+                        stack.pop()
+                        break
+                        
+                    # get 3 elements in ele
+                    while len(ele) < 3:
+                        ele.append(stack.pop())
+                    
+                    # assert middle element is operator
+                    # assert surrounding elements are nums
+                    assert(ele[1] in op)
+                    assert(type(ele[2]) == int)
+                    assert(type(ele[0]) == int)
+                    
+                    val = evaluate(ele[1], ele[0], ele[2])
+                    stack.append(val)
+                    
+                    # reset ele for next operation
+                    # for example (7 - 8 + 9) is evaluated as 9 + 8 - 7, so after doing 7-8 =-1 and adding this "val" to stack
+                    # we need to reset ele, so that we can do 9 + (-1) next time
+                    ele = []
+                    
+                
+                if ele:
+                    assert(len(ele) == 1)
+                    stack.append(ele[0])
+                
+                i +=1
+                continue
+            
+        return stack[-1]
---
+++ Python/BasicCalculator2.py
@@ -0,0 +1,89 @@
+# # https://leetcode.com/problems/basic-calculator-ii
+class Solution:
+    def calculate(self, s: str) -> int:
+        s = s.replace(' ', '')
+        op = ['/', '*', '+', '-']
+
+        # if input is like "1"        
+        if s.isdigit():
+            return int(s)
+        
+        def getRightNum(s, idx, returnWithoutSign=False):
+            # find second num
+            second = ''
+            j = idx + 1
+            neg = False
+            if s[j] == '-':
+                neg = True
+                j +=1
+
+            while j < len(s) and s[j] not in op:
+                second += s[j]
+                j +=1
+
+            second = int(second)
+            if neg:
+                second = -second
+            
+            if returnWithoutSign:
+                return abs(second)
+            
+            return second
+        
+        def getLeftNum(s, idx, returnWithoutSign=False):
+            # find first num
+            first = 0
+            j = idx - 1
+            neg = False
+            p = 0
+            while j >= 0 and s[j] not in op:
+                first += int(s[j]) * 10**p
+                p +=1
+                j -=1
+
+            if j >=0 and s[j] == '-':
+                neg = True
+            if neg:
+                first = -first
+            
+            if returnWithoutSign:
+                return abs(first)
+            
+            return first
+        
+        stack = []
+        for i in range(len(s)):
+            if s[i].isdigit():
+                continue
+            else:
+                # it is an operator
+                assert(s[i] in op)
+                if stack:
+                    first = stack.pop()
+                else:
+                    first = getLeftNum(s, i, True)
+                
+                second = getRightNum(s, i, True)
+                
+                # if operators are div or mul, we want to eagerly calculate and ingest on stack top
+                if s[i] in ['*', '/']:
+                    if s[i] == '*':
+                        res = first * second
+                    else:
+                        res = int(first / second)
+                    stack.append(res)
+                else:
+                    # operator is addition or subtraction, we want to evaluate this lazily
+                    # because there may be additional div or mul ahead in the string (BODMAS)
+                    
+                    # if sign is negative, then just ingest -ve of second num into stack
+                    # why? because "2+5-3" ==> [2, 5, -3] and we can just return the sum of the stack
+                    # we're summing the stack and that's fine because 2+5-3 is the same as 2 + 5 + (-3)
+                    if s[i] == '-':
+                        second = -1 * second
+                    # order is important here in terms of insertion into stack
+                    # because we want second to be top of stack
+                    stack.append(first)
+                    stack.append(second)
+        
+        return sum(stack)
