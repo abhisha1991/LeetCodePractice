6c555a2dd70bb0ae1fe3fb232fa6d69268fdf71d
unknown <aus2101@columbia.edu>
2022-04-26 23:42:11 -0700

    more soln

---
+++ Python/ConcatenatedWords.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/concatenated-words
+class Solution:
+    def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
+        wordset = set()
+        for w in words:
+            wordset.add(w)
+        
+        res = []
+        def dfs(word):
+            for i in range(1, len(word)):
+                left = word[:i]
+                right = word[i:]
+                # check if left and right are in wordset
+                # if not, check which one is and do a dfs for the other
+                if left in wordset and right in wordset:
+                    return True
+                elif left in wordset and dfs(right):
+                    return True
+                elif right in wordset and dfs(left):
+                    return True
+                
+            return False
+        
+        for w in words:
+            if dfs(w):
+                res.append(w)
+            
+        return res
