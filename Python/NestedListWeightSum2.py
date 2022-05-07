b1e010a485cf21b5902e337eae410497d732c9b5
unknown <aus2101@columbia.edu>
2022-05-07 14:58:27 -0700

    more soln

--- Python/NestedListWeightSum2.py
+++ Python/NestedListWeightSum2.py
@@ -1,4 +1,4 @@
-# https://leetcode.com/problems/nested-list-weight-sum-ii/
+# https://leetcode.com/problems/nested-list-weight-sum-ii
 # """
 # This is the interface that allows for creating nested lists.
 # You should not implement it, or speculate about its implementation
@@ -46,8 +46,47 @@ class Solution:
     def __init__(self):
         self.res = 0
         self.maxD = 0
+        self.nums = []
         
+    # this does a single pass of dfs
+    '''
+    we need to find sum(A_i * Weight_i)
+    this can be written as sum(A_i * {maxDepth - depth_i + 1})
+    this can be written as sum(A_i * (maxDepth + 1) - A_i * (depth_i))
+    this can be written as sum(A_i * (maxDepth + 1)) - sum(A_i * (depth_i))
+    
+    this can be written as (maxDepth+1) * sum(A_i) - sum(A_i * depth_i)
+                           |_____________________|   |_______________|
+                                PART A                   PART B
+                           
+    by re-arranging like this, we can find maxDepth for part A and part B together in a single dfs pass
+    '''
     def depthSumInverse(self, nestedList: List[NestedInteger]) -> int:
+        self.helperOptimal(nestedList, 1)
+        ans = (self.maxD+1) * sum(self.nums) - self.res
+        print(f"maxD is {self.maxD}")
+        print(f"nums is {self.nums}")
+        print(f"res is {self.res}")
+        
+        return ans
+    
+    def helperOptimal(self, nl, depth):
+        for element in nl:
+            if element.isInteger():
+                e = element.getInteger()
+                # part A
+                self.maxD = max(self.maxD, depth)
+                self.nums.append(e)
+                # part B
+                self.res += depth * e
+            else:
+                self.helperOptimal(element.getList(), depth+1)
+    
+    # this does 2 passes of dfs, so its still o(n) but we're doing 2 passes
+    # why o(n)? because we're doing one pass to calculate max depth, which takes o(n)
+    # and another pass to do the result calculation, which also takes o(n)
+    # this passes all test cases, but is not "elegant"
+    def depthSumInverse2(self, nestedList: List[NestedInteger]) -> int:
         self.helperMaxD(nestedList, 1)
         self.helper(nestedList, 1)
         return self.res
@@ -55,7 +94,7 @@ class Solution:
     def helper(self, nl, depth):
         for element in nl:
             if element.isInteger():
-                # this is the weight sum we're supposed to calculate, where weight = max depth - curr depth + 1
+                # this is the weight we're supposed to calculate, weight = max depth - curr depth + 1
                 self.res += (self.maxD - depth + 1) * element.getInteger()
             else:
                 self.helper(element.getList(), depth+1)
