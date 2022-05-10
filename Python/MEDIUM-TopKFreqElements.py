c0eb60d134851eae1077f0054bb85be12bc4b9ff
unknown <aus2101@columbia.edu>
2022-05-10 11:18:04 -0700

    more soln

--- Python/MEDIUM-TopKFreqElements.py
+++ Python/MEDIUM-TopKFreqElements.py
@@ -1,79 +1,35 @@
 # https://leetcode.com/problems/top-k-frequent-elements/description/
-from collections import defaultdict
-import time
-
-
-# Not optimal, but a pretty good solution
+import heapq
 class Solution:
-    def __init__(self):
-        self.ans = []
-
-    def topKFrequent(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
-        self.top_k_util(nums, k)
-        return self.ans
-
-    def top_k_util(self, nums, k):
-        # print("nums is", nums)
-        # print("k is ", k)
-        dic = defaultdict(int)
-        maxk = 0
-        maxv = 0
-        for i in nums:
-            dic[i] += 1
-            if maxv < dic[i]:
-                maxv = dic[i]
-                maxk = i
-
-        self.ans.append(maxk)
-        nums = [x for x in nums if x != maxk]
-        if k > 1:
-            self.top_k_util(nums, k - 1)
-        return
-
-# Actual correct solution, pretty simple to follow
-class Solution2:
-    def __init__(self):
-        self.ans = []
-
-    def topKFrequent(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
-        # here dic just captures frequency of each element in the list
-        # key = element in the list, value = its frequency in the list
-        dic = defaultdict(int)
-        for i in nums:
-            dic[i] += 1
-
-        # bucket essentially flips the equation
-        # it stores frequency as key, and the element of the list as the value
-        bucket = defaultdict(list)
-        for x, v in dic.items():
-            if len(bucket[v]) == 0:
-                bucket[v] = [x]
-            else:
-                bucket[v].append(x)
-
-        print("bucket", bucket)
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         res = []
+        dic = dict()
+        revDic = defaultdict(list)
+        h = []
+        heapq.heapify(h)
+        
+        # create a frequency counter for the with nums[i] as keys
+        for i in nums:
+            if i not in dic:
+                dic[i] = 1
+            else:
+                dic[i] +=1
+        
+        # create a reverse dictionary with key = frequency,
+        # value is going to be the nums[i] that had that frequency
+        for key, val in dic.items():
+            revDic[val].append(key)
+        
+        # insert the frequencies into a max heap
+        for v in revDic.keys():
+            heapq.heappush(h, -v)
+        
+        # till the size of result is k, keep taking max frequency out of heap
+        # and keep adding to res the nums[i] that have this highest frequency
         while len(res) < k:
-            # find the max frequency
-            maxk = max(bucket)
-            print("maxk", maxk)
-            # add the element of the original list with max frequency to result
-            res.extend(bucket[maxk])
-            print("res", res)
-            # delete the bucket entry with the highest frequency so you can pick the next highest frequency
-            del bucket[maxk]
-
-        return res[0:k]
-
-s2 = Solution2()
-s2.topKFrequent([1,1,1,2,2,3], 2)
+            v = -1 * heapq.heappop(h)
+            vals = revDic[v]
+            res.extend(vals)
+        
+        # return top k elements
+        return res[:k]
