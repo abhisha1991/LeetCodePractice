ab1655939f39ecba849f912da22846b6320a8786
unknown <aus2101@columbia.edu>
2022-05-15 14:23:00 -0700

    more soln

--- Python/MinAddToMakeParanthesisValid.py
+++ Python/MinAddToMakeParanthesisValid.py
@@ -6,19 +6,23 @@ class Solution:
         
         ans = 0
         i = 0
+        # if string starts with close paranthesis, keep adding to ans since we'd need to add open paranthesis to counter act
         while i < len(s) and s[i] == ')':
             ans +=1
             i +=1
         
         stack = []
+        # at this point, i points to an open paranthesis
         while i < len(s):
             if s[i] == '(':
                 stack.append(i)
             else:
+                # encoutered a closed paranthesis
                 # balance out with open paranthesis that are there in stack
                 if stack:
                     stack.pop()
                 else:
+                    # if not stack means that we've run out of open paranthesis but are still encountering closed paranthesis
                     # excess closing paranthesis means we need to add open paranthesis, ie, increment ans
                     ans +=1
             i +=1
