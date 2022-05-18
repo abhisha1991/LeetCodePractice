2e4e09e5cf7df5bb7834f69d9b607876d1653919
unknown <aus2101@columbia.edu>
2022-05-18 01:20:46 -0700

    more soln

--- Python/SimplifyPath.py
+++ Python/SimplifyPath.py
@@ -1,40 +1,28 @@
-# https://codefights.com/interview/wqveBLtNtfByaEnfW
-# TODO: Not complete
-import re
-
-
-def simplifyPath(path):
-    '''
-    Here is how this path was simplified:
-    * /./ means "move to the current directory" and can be replaced with a single /;
-    * /x/../ means "move into directory x and then return back to the parent directory", so it can replaced with a single /;
-    * // means "move to the current directory" and can be replaced with a single /.
-    '''
-    if (path == '/'):
-        return path
-
-    if (path[0] != '/'):
-        path = '/' + path
-
-    while '.' in path:
-        path = path.replace('/./', '/')
-        path = path.replace('//', '/')
-        path = re.sub(r'\/[a-z]*\/\.\.\/', '/', path)
-        path = re.sub(r'\/[a-z]*\/\.\.', '/', path)
-        path = re.sub(r'\/\.\.\/', '/', path)
-
-    print(path)
-    if (path == '/'):
-        return path
-
-    if (path[len(path) - 1] is '/'):
-        path = path[:-1]
-
-    print(path)
-    return path
-
-
-# simplifyPath("/a/b/c/../..")
-# simplifyPath("home/a/./x/../b//c/")
-# simplifyPath("/../")
-simplifyPath("a/b/../c/d/../../e/../../")
+# https://leetcode.com/problems/simplify-path/
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+        
+        parts = path.split('/')
+        for p in parts:
+            # its only in this case where we need to move one directory up
+            # ie, we need to pop from the stack
+            if p == '..':
+                if stack:
+                    stack.pop()
+            
+            # the case of empty string will occur when we have something like a//b, 
+            # when we split by '/', we get ['a', '', 'b']
+            # when we have a period, like a/./b ==> ['a', '.', 'b']
+            # we are just saying that we want to stay on the present directory, so its a no-op 
+            elif p == '' or p == '.':
+                continue
+            
+            else:
+                # if we have a legit folder name like 'a' or 'b'
+                # then insert that into stack
+                stack.append(p)
+        
+        # remember to add '/' in the front of the string as all paths must begin with it
+        # all elements of the stack, which are now directory names, must be joined with '/'
+        return '/' + '/'.join(stack)
