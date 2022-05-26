3801388f851da6cffef28a50ec26023fe380febf
unknown <aus2101@columbia.edu>
2022-05-25 20:57:05 -0700

    more soln

--- Python/BusRoutes.py
+++ Python/BusRoutes.py
@@ -12,24 +12,33 @@ class Solution:
                 graph[stop].append(bus)
         
         
+        # the q contains a tuple of 2 elements (stop to explore, current set of buses we have been on)
         q = deque([(source, set())])
-        visited = set()
+        # if we've already explored a stop, we add to this set
+        stopVisited = set()
+        # if we've already explored a bus, we add to this set
+        busVisited = set()
         
         while q:
             stop, bset = q.popleft()
             
             if stop == target:
+                # return length of buses we have onboarded so far
                 return len(bset)
             
-            visited.add(stop)
+            stopVisited.add(stop)
             
             for b in graph[stop]:
+                if b in busVisited:
+                    continue
+                    
+                busVisited.add(b)
+                
+                # add this new bus to take to a new set
                 bsetNew = set(bset)
                 bsetNew.add(b)
+                # explore all stops for this bus
                 for s in routes[b]:
-                    if s == target:
-                        return len(bsetNew)
-                    
-                    if s not in visited:
+                    if s not in stopVisited:
                         q.append((s, bsetNew))
         return -1
