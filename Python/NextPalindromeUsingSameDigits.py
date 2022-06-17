5667a19c2cc14bb0f581cb66d03b427a365956a1
unknown <aus2101@columbia.edu>
2022-06-17 15:58:10 -0700

    more soln

--- Python/NextPalindromeUsingSameDigits.py
+++ Python/NextPalindromeUsingSameDigits.py
@@ -6,13 +6,27 @@ class Solution:
     def nextPalindrome(self, num: str) -> str:
         nums = [num[i] for i in range(len(num))]
         
+        # nums with length 1 cannot have next greater palindrome
         if len(nums) == 1:
             return ""
         
-        # find pos from right where the decreasing criteria breaks
+        # since nums is a palindrome (given), we can focus only on first half of the number
+        n = len(nums)
+        # check if its an even length palindrome or not
+        even = True if n % 2 == 0 else False
+        
+        # if its not an even length palindrome, get the mid element and store separately
+        mid = nums[n//2] if not even else None
+        
+        # now let's deal only with the first half of nums, so lets shrink it to half and
+        # go with the assumption as if this was our original number
+        m = n//2
+        nums = nums[:m]
+        
+        # find pos from right where the decreasing criteria breaks (this is borrowed from NextGreaterElement-III)
         # example, 564987 --> from the right, decreasing criteria breaks at digit 4, ie, pos = 2 
         pos = None
-        for i in range(len(nums)-1, -1, -1):
+        for i in range(m-1, -1, -1):
             prev = nums[i-1]
             cur = nums[i]
             # if cur happens to be greater than the previous digit, then the criteria is broken
@@ -30,27 +44,27 @@ class Solution:
         nxtGreaterPos = None
         mn = sys.maxsize
         
-        for i in range(pos+1, len(nums)):
-            # nums[i] is a char, so take ord (ascii rep) to find diff
+        for i in range(pos+1, m):
             diff = ord(nums[i]) - ord(nums[pos])
             if diff > 0 and mn > diff:
                 mn = diff
                 nxtGreaterPos = i
         
-        # swap pos and nxtGreaterPos
-        # in example above: 564987, pos = 2, ie nums[2] = 4, nxtGreaterPos = 5, ie, nums[5] = 7
-        # swap 4 and 7 --> 567984 -- this is not the next greatest though
+        # swap the next greater number and the number at pos
         tmp = nums[pos]
         nums[pos] = nums[nxtGreaterPos]
         nums[nxtGreaterPos] = tmp
         
-        # we need to sort the stuff after pos+1, ie, [5,6,7] + [4,8,9] --> 567489 
-        # -- this is next greatest after 564987
-        nums = nums[:pos+1] + sorted(nums[pos+1:])
-        
-        # palindrome check since we want to only return palindromes
-        if nums == nums[::-1]:
-            return "".join(nums)
+        # if we're not dealing with even length palindrome, we need to add 'mid' to the middle of nums, else we dont
+        # but generally, we need to make nums back to its original size of 'n' instead of shortened half size 'm'
+        # so we need to do something like nums = A + reversed(A)
+        if not even:
+            nums = nums[:pos+1] + sorted(nums[pos+1:])
+            rev = list(reversed(nums))
+            nums = nums + [mid] + rev
         else:
-            # call recursively till you find the next greater element which is a palindrome (or return empty)
-            return self.nextPalindrome("".join(nums))
+            nums = nums[:pos+1] + sorted(nums[pos+1:])
+            rev = list(reversed(nums))
+            nums = nums + rev
+        
+        return "".join(nums)
