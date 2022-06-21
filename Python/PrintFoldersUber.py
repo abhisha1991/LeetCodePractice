0bb0b1b079a5577070fb8805bc5e59aad4cbeb89
unknown <aus2101@columbia.edu>
2022-06-21 15:45:03 -0700

    more soln

---
+++ Python/PrintFoldersUber.py
@@ -0,0 +1,79 @@
+'''
+Given list of folders, print the path of a given folder from root. 
+If there is no path to the root folder then return an empty string. 
+Root level folders will have 0 as id. 
+The structure of Folder is like this:
+
+class Folder {
+   int id;
+   List<int> subfolders;
+   String name;
+}
+
+Ex: 
+Solution([Folder(0, [7, 3], "abc"), Folder(0, [11], "xyz"), Folder(3, [100], "pqr"), 
+Folder(8, [], "def"), Folder(7, [9], "ijk"), Folder(9, [], "lmn"), Folder(11,[],"ijk"), 
+Folder(100, [], "pqrs")])
+
+Clarification: There can be multiple root level folders. All other folders have unique ids.
+Note: printPath method may be called multiple times. Design your solution taking that into account
+'''
+class Folder:
+    def __init__(self, idx, subfolders, name):
+        self.idx = idx
+        self.subfolders = subfolders
+        self.name = name
+        self.sub = dict()
+
+class Solution():
+    def __init__(self, folderList):
+        self.parents = []
+        self.nonparents = dict()
+        for f in folderList:
+            if f.idx == 0:
+                self.parents.append(f)
+            else:
+                self.nonparents[f.idx] = f
+        
+        self.buildTrie(self.parents)
+    
+    def buildTrie(self, parents):
+        for f in parents:
+            childList = []
+            for ff in f.subfolders:
+                child = self.nonparents[ff]
+                f.sub[ff] = child
+                childList.append(child)
+            
+            self.buildTrie(childList)
+        
+        
+    def printPath(self, num):
+        if num == 0:
+            return ""
+
+        self.res = []
+            
+        def dfs(node, num, path):
+            if node.idx == num:
+                self.res = path + [self.nonparents[num].name]
+                return
+            
+            for ff in node.sub.values():
+                dfs(ff, num, path + [node.name])
+                                
+        for f in self.parents:
+            dfs(f, num, [])
+        
+        return self.res
+
+s = Solution([Folder(0, [7, 3], "abc"), Folder(0, [11], "xyz"), Folder(3, [100], "pqr"), Folder(8, [], "def"), Folder(7, [9], "ijk"), Folder(9, [], "lmn"), Folder(11,[],"ijk"), Folder(100, [], "pqrs"), ])
+
+print(s.printPath(9))
+print(s.printPath(11))
+print(s.printPath(7))
+print(s.printPath(3))
+print(s.printPath(100))
+
+
+       
