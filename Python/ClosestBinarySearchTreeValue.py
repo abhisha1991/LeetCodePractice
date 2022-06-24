d4b28fc94b93cf2715efde8378cd4739d7432cf4
unknown <aus2101@columbia.edu>
2022-06-23 23:51:19 -0700

    more soln

---
+++ Python/ClosestBinarySearchTreeValue.py
@@ -0,0 +1,44 @@
+# https://leetcode.com/problems/closest-binary-search-tree-value
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+import bisect
+class Solution:
+    # this is an o(n) solution since we're having to construct the binary search tree into an arr
+    def closestValue(self, root: Optional[TreeNode], target: float) -> int:
+        self.arr = []
+        
+        def helper(node):
+            if not node:
+                return
+
+            # always remember that inorder of a bst gives you sorted arr
+            helper(node.left)
+            self.arr.append(node.val)
+            helper(node.right)
+        
+        
+        helper(root)
+        left = bisect.bisect_left(self.arr, target)
+        right = bisect.bisect_right(self.arr, target)
+        
+        # the target number exactly exists in arr
+        if left != right:
+            return self.arr[left]
+        
+        # edge case, target number is smaller than all other numbers
+        if left == 0:
+            return self.arr[left]
+        
+        # edge case, target number is larger than all other numbers
+        if left == len(self.arr):
+            return self.arr[left-1]
+        
+        # regular case, we need to check left-1 and left
+        # see which one has a lesser abs diff from target and return that num
+        if abs(target - self.arr[left-1]) < abs(target - self.arr[left]):
+            return self.arr[left-1]
+        return self.arr[left]
