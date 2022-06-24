4289aac57d0af9836e49a2093504a9c8ac8d68e8
unknown <aus2101@columbia.edu>
2022-06-23 22:14:54 -0700

    more soln

--- Python/RandomPickIndex.py
+++ Python/RandomPickIndex.py
@@ -1,30 +1,16 @@
 # https://leetcode.com/problems/random-pick-index
-class Solution(object):
+from collections import defaultdict
+import random
+class Solution:
 
-    def __init__(self, nums):
-        """
-        :type nums: List[int]
-        """
-        self.nums = nums
+    def __init__(self, nums: List[int]):
+        self.count = 0
+        self.dic = defaultdict(list)
+        for i in range(len(nums)):
+            self.dic[nums[i]].append(i)
 
-    def pick(self, target):
-        """
-        :type target: int
-        :rtype: int
-        """
-        count = 0
-        index = []
-        for idx, num in enumerate(self.nums):
-            if num == target:
-                count +=1
-                index.append(idx)
-        
-        # now count holds the number of times we have seen that target number
-        # the idea is that if we were to do random.randrange(count) - we will get a number between 0 and count
-        # say count was 3, then we get a number from [0,1,2] with equal likelihood
-        r = random.randrange(count)
-        return index[r]
-
+    def pick(self, target: int) -> int:
+        return random.choice(self.dic[target]) 
 
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(nums)
